2e17be87ce000673a18786dc2359631636ad8eff
Vijay Singh Rathore <vijayrathore2003@gmail.com>
2024-04-28 22:10:33 +0530

    again fixed deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['swiper/react']
+      external: ['swiper/react', 'swiper/css'] // Add 'swiper/css' here
     }
   }
 })
