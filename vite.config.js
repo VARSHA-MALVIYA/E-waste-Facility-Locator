71f9465831331c5aa0217bf1e0308ac076f7d7e6
Vijay Singh Rathore <vijayrathore2003@gmail.com>
2024-04-28 22:21:00 +0530

    deploy fix swiper install

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['swiper/react', 'swiper/css','swiper/css/pagination','swiper/modules'] // Add 'swiper/css' here
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['swiper/react', 'swiper/css','swiper/css/pagination','swiper/modules'] // Add 'swiper/css' here
+  //   }
+  // }
 })
